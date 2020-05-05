f48bb6f8718394358e9adf86c3ef30ff6e7be77a
huzaifa8 <huzaifa.javaid8@gmail.com>
2020-05-05 06:54:33 +0500

    foreign key

--- store/products/models.py
+++ store/products/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
 
+class Site(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     price = models.FloatField()
     desc = models.TextField()
     img = models.ImageField(upload_to='pics')
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
+
+
+
 
 
 
--- store/products/views.py
+++ store/products/views.py
@@ -3,13 +3,12 @@ from .models import Product
 from .filters import Filter
 
 
-
 def products_view(request):
     context = Product.objects.all()
-    myFilter = Filter(request.GET, queryset=context)
-    context = myFilter.qs
+    myfilter = Filter(request.GET, queryset=context)
+    context = myfilter.qs
     return render(request, 'main.html', {'products': context,
-                                         'filter': myFilter})
+                                         'filter': myfilter})
 
 
 
